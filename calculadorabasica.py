0085ec06732494661bb22f2e84a823712035e27d
Raul Zuriel Martinez Ramirez <raulzury@MacBook-Pro-de-Raul.local>
2022-12-23 22:28:58 -0600

    Empece una calculadora basica

---
+++ calculadorabasica.py
@@ -0,0 +1,9 @@
+print("Esta es una calculadora básica")
+
+try:
+    a2 = int(input("Añade tu primer número:" ))
+    
+    a3 = int(input("Añade tu segundo número:" ))
+
+except:
+    print("Asi no animal")
